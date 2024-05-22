2452a36b7730e31b2ace83896408ce30560a4154
gomezc <chayanon.p@mail.bbvc.ac.th>
2024-05-22 11:26:51 +0700

    3rd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,57 @@
+import { Col, Container, Nav, Navbar, NavbarBrand } from 'react-bootstrap';
 import {sampleProducts} from './data'
-import './App.css'
+import './index.css'
+
 
 function App( ){
   
 
   return (
-    <>
-    <header>ShopG</header>
+    <div className='d-flex flex-column vh-100 '>
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg'>
+      <Container>
+        <NavbarBrand>ShopG</NavbarBrand>
+      </Container>
+      <Nav>
+        <a className='nav-link' href="/cart">Cart</a>
+        <a className='nav-link' href="/cart">Sign In</a>
+      </Nav>
+      </Navbar>
+    </header>
     <main>
-      <ul>
-        {sampleProducts.map((product)=>
+      <div className="container mt-3 mb-3">
+        <div className="row">
+
         
-        <li key={product.slug}>
-          <img className='product-image' src={product.image} alt="" />
-          <h2>{product.name}</h2>
+        {sampleProducts.map((product)=>
+
+        <Col key={product.slug} sm={6} md={4} lg={3}>
+         
+          <img className='product-image rounded' src={product.image} alt="" />
+          <h3><b>                                             </b></h3>
           <p>${product.price}</p>
-        </li>
+    
+        </Col>
+       
         
         )}
-      </ul>
+      
+      </div>
+      </div>
+      
     </main>
-    <footer>AlL ri</footer>
-    </>
+    <footer>
+      <div className="text-center bg-dark text-white">
+
+        All right Reserved
+        
+      </div>
+     
+      
+      
+      </footer>
+    </div>
   )
 }
 
